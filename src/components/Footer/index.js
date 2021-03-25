dce5ff237ceeafec828fc3ddf3f91b405c7744d6
Said Mg <saidmghabghab@gmail.com>
2021-03-24 20:49:09 -0400

    Footer Component done

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+    return <div className = "border-top border-light" style= {{backgroundColor:"#343a40", color:"white", position: "fixed", width:"100% ",
+    textAlign:"center", bottom:"0%"}}>
+        <h4> &#169; Copyright 2021 | Said Mghabghab</h4>
+    </div>
+}
+
+export default Footer;
